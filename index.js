f2a2324e8621a6f520020c7e31191ad0212cfdbf
shzfas <yuraka200486@gmail.com>
2023-03-02 01:22:27 +0600

    feat: upload photos on server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import multer from 'multer';
 import mongoose from 'mongoose';
 import { UserController } from './controllers/index.js';
 import { loginValidation, registerValidation } from './validations.js';
@@ -11,13 +13,34 @@ mongoose
   .catch((err) => console.log('DB error', err));
 
 const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
 app.use(express.json());
 app.use(cors());
+app.use('/uploads', express.static('uploads'));
 
 /* Регистрация учителя */
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+/* Загрузка изображений на сервер */
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
