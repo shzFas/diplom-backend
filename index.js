b9ee66109fc02b8d9c3e064efdee64ccecd55b3a
shzfas <yuraka200486@gmail.com>
2023-03-02 00:39:16 +0600

    feat: make endpoint for register (Teacher)

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+/* Регистрация учителя */
+app.post('/auth/login');
+app.post('/auth/register');
+app.get('/auth/me');
+
 app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
