9b37b74cf0d419d2626c669ce560f96c01ac7fee
shzfas <yuraka200486@gmail.com>
2023-03-02 22:34:34 +0600

    feat: create model for Mark

---
+++ models/Mark.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const MarkSchema = new mongoose.Schema(
+  {
+    markTeacher: {
+      type: String,
+      required: true,
+    },
+    markPredmet: {
+      type: String,
+      required: true,
+    },
+    markStudent: {
+      type: String,
+      required: true,
+    },
+    markClassStudent: {
+      type: String,
+      required: true,
+    },
+    markFalse: {
+      type: Boolean,
+      required: true,
+    },
+    markValue: {
+      type: Number,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('Mark', MarkSchema);
